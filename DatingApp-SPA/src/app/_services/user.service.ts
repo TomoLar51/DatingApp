dc7a2a491c42eec4abbddd81b05b1541c8712e13
Thomas Larsen <tomo@tomosoft.dk>
2020-04-24 23:29:49 +0200

    End of section 10

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -8,15 +8,19 @@ import { User } from '../_models/user';
     providedIn: 'root',
 })
 export class UserService {
-    bareUrl = environment.apiUrl;
+    baseUrl = environment.apiUrl;
 
     constructor(private http: HttpClient) {}
 
     getUsers(): Observable<User[]> {
-        return this.http.get<User[]>(this.bareUrl + 'users');
+        return this.http.get<User[]>(this.baseUrl + 'users');
     }
 
     getUser(id): Observable<User> {
-        return this.http.get<User>(this.bareUrl + 'users/' + id);
+        return this.http.get<User>(this.baseUrl + 'users/' + id);
+    }
+
+    updateUser(id: number, user: User) {
+        return this.http.put(this.baseUrl + 'users/' + id, user);
     }
 }
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -20,9 +20,13 @@ import { ListsComponent } from './lists/lists.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
 import { MessagesComponent } from './messages/messages.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
+
 
 
 export function tokenGetter() {
@@ -40,6 +44,7 @@ export function tokenGetter() {
       MemberListComponent,
       MemberCardComponent,
       MemberDetailComponent,
+      MemberEditComponent,
       MessagesComponent
    ],
    imports: [
@@ -63,7 +68,9 @@ export function tokenGetter() {
       AuthService,
       ErrorInterceptorProvider,
       MemberDetailResolver,
-      MemberListResolver
+      MemberListResolver,
+      MemberEditResolver,
+      PreventUnsavedChanges
    ],
    bootstrap: [
       AppComponent
